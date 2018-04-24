20ee54d86a239bfd403e73f75bc2dfedd40f416e
zp <925616096@qq.com>
2018-04-24 23:49:20 +0800

    redis mongodb

--- spring-boot-demo/src/main/java/com/roc/springboot/spring_boot_demo/SpringbootApplication.java
+++ spring-boot-demo/src/main/java/com/roc/springboot/spring_boot_demo/SpringbootApplication.java
@@ -1,26 +1,20 @@
 package com.roc.springboot.spring_boot_demo;
 
 import java.util.Arrays;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import com.roc.springboot.spring_boot_demo.ex5.Customer;
-import com.roc.springboot.spring_boot_demo.ex5.CustomerDAO;
-import com.roc.springboot.spring_boot_demo.ex6.RedisDAO;
-
 @SpringBootApplication
 public class SpringbootApplication {
 
-	@Autowired
+/*	@Autowired
 	RedisDAO redisDAO;
 	@Autowired
-	CustomerDAO customerDAO;
+	CustomerDAO customerDAO;*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(SpringbootApplication.class, args);
@@ -28,35 +22,35 @@ public class SpringbootApplication {
 	
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//    	Redis
-		redisDAO.setValue("zhang","peng");
-		redisDAO.setValue("huang","jing");
-		 // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        
-        String value = redisDAO.getValue("huang");
-    	System.out.println(value);
-    	
-//    	Mongodb
-		// TODO Auto-generated method stub
-		customerDAO.deleteAll();
-		customerDAO.save(new Customer("zhang","peng"));
-		customerDAO.save(new Customer("huang","jing"));
-		 // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        
-        List<Customer> customers = customerDAO.findAll();
-        for(Customer customer : customers) {
-        	System.out.println(customer.toString());
-        }
-        System.out.println("Customer found with findByFirstName('zhang'):");
-        System.out.println("--------------------------------");
-        customers = customerDAO.findByFirstName("zhang");
-        for(Customer customer : customers) {
-        	System.out.println(customer.toString());
-        }
+////    	Redis
+//		redisDAO.setValue("zhang","peng");
+//		redisDAO.setValue("huang","jing");
+//		 // fetch all customers
+//        System.out.println("Customers found with findAll():");
+//        System.out.println("-------------------------------");
+//        
+//        String value = redisDAO.getValue("huang");
+//    	System.out.println(value);
+//    	
+////    	Mongodb
+//		// TODO Auto-generated method stub
+//		customerDAO.deleteAll();
+//		customerDAO.save(new Customer("zhang","peng"));
+//		customerDAO.save(new Customer("huang","jing"));
+//		 // fetch all customers
+//        System.out.println("Customers found with findAll():");
+//        System.out.println("-------------------------------");
+//        
+//        List<Customer> customers = customerDAO.findAll();
+//        for(Customer customer : customers) {
+//        	System.out.println(customer.toString());
+//        }
+//        System.out.println("Customer found with findByFirstName('zhang'):");
+//        System.out.println("--------------------------------");
+//        customers = customerDAO.findByFirstName("zhang");
+//        for(Customer customer : customers) {
+//        	System.out.println(customer.toString());
+//        }
         
         return args -> {
 
